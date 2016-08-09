5de507dba20dec04e7ca7e3c94e2b19ff5fe720d
Alik Geller <alikgeller@gmail.com>
2016-08-09 12:07:09 +0300

    simple client

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 var https = require('https');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var options = {
    key  : fs.readFileSync('server.key'),
    cert : fs.readFileSync('server.crt')
 };
 
-app.get('/', function (req, res) {
-   res.send('Easy Broadcast - Encryption Broadcasting');
-});
+app.use(express.static(__dirname + '/public'));
+
 
 https.createServer(options, app).listen(3000, function () {
    console.log('Started!');
---
+++ public/Conttrollers/controllers.js
@@ -0,0 +1,3 @@
+function appctr(){
+	console.log("Hello world");
+}
