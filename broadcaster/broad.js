c954de393438906d1524972e96b8fc35ea176182
alikgeller <alikgeller@gmail.com>
2016-09-21 20:31:55 +0300

    Complete server and client

--- broadcaster/broad.js
+++ broadcaster/broad.js
@@ -1,35 +1,57 @@
 
 //most include path to FFmpeg appliction on the fulent-ffmpeg module
 
-
-var dgram = require('dgram');
-var socket = dgram.createSocket('udp4');
-
 var ffmpeg = require('fluent-ffmpeg');
-
 var fs = require('fs');
+var dgram = require('dgram');
+var dgram2 = require('dgram');
+var socket = dgram.createSocket('udp4');
+var socket2 = dgram2.createSocket('udp4');
+
+
+
+
+var RtpPacket=require("./RtpPacket.js");
+
+
+//parameters
 
 var testmessage = "[hello broadcaster] pid: ";
-
-var address = '192.168.0.255'; //change for your LAN
+var address = '192.168.0.255'; //change for your LAN broacast IP !
 var port = 5000;
+var portinit = 5556;
 var host = '127.0.0.1';
 
-var RtpSession=require("rtp-rtcp").RtpSession;
-var RtpPacket=require("rtp-rtcp").RtpPacket;
- 
+
+
 //server
- 
-var r1=new RtpSession(5556);
-//var r2 = new RtpSession(5557);
-//r2.setRemoteAddress(5558,"127.0.0.1");
- 
-r1.on("listening",function(){
-    console.log("rtp server is listening...");
+
+socket2.on('listening', function () {
+    var address = socket2.address();
+    console.log('RTP Server listening on ' + address.address + ":" + address.port);
 });
 
-socket.bind(port);
+
+socket2.on('message', function(msg, rinfo){
+	  var rtpPacket = new RtpPacket(msg);
+	  
+	  socket.send(rtpPacket.getBuffer(), 
+	  0,
+	  rtpPacket.getBuffer().length,
+	  port,
+	  host,
+	  function(err){
+	    if (err) console.log(err);
+	  });   
+});
  
+
+socket.bind(9999);
+socket2.bind(portinit); 
+
+
+
+
 
 function sendmessage (){
   socket.send(new Buffer(testmessage), 
@@ -46,74 +68,22 @@ function sendmessage (){
 
 
 
-r1.on("message",function(msg,info){
-    var rtpPacket = new RtpPacket(msg);
-    //console.log(rtpPacket.getSeqNumber().toString()+" from "+info.address+" "+info.port);
-    //r2.sendPacket(rtpPacket,msg.length);
-    socket.send(rtpPacket.getBuffer(), 
-    0,
-    rtpPacket.getBuffer().length,
-    port,
-    address,
-    function(err){
-      if (err) console.log(err);
-    });
-});
-
-
-
-
-/*
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('addr: '+add);
-  host=add;
-})
-
-var rtptext = fs.createWriteStream("received.json",{ flags: 'w',
-  encoding: "utf8",
-  mode: 0666 });
-
-
-
-socket.on('listening', function(){
-    var address = socket.address();
-    console.log("listening on :" + address.address + ":" + address.port);
-});
-
-socket.on('message', function (message, remote) {
-    rtptext.write(message);
-
-});
-*/
-
-
-
-// setInterval(sendmessage,2000);
-
-
-
- 
-//server
- 
 
 
 
 
 
 
+//RTP creation with FFmpeg
 
-
-
-
-
-
-
-var proc = ffmpeg('SampleVideo2.ts').native()
-  // use the 'flashvideo' preset (located in /lib/presets/flashvideo.js)
-  .videoCodec('copy')
-  .noAudio()
+var proc = ffmpeg('SampleVideo.mp4').native()
   
-  .outputOptions('-f rtp')
+  .videoCodec('mpeg2video')
+  .videoBitrate('1024k')
+  .audioCodec('libmp3lame')
+  .audioBitrate('128k')
+  
+  .outputOptions('-f rtp_mpegts')
 
   .output('rtp://127.0.0.1:5556')
   
@@ -134,16 +104,3 @@ var proc = ffmpeg('SampleVideo2.ts').native()
   // save to stream
   .run();
 
-
-
-
-
-
-/*
-
-socket.bind(function(){
-	socket.setBroadcast(true);
-	setInterval(sendmessage,1000);
-});
-
-*/
